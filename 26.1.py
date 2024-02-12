28fd0a298a56a3c4dd745016a4106cc3449f4c78
IhorVG <igapyak@gmail.com>
2024-02-12 16:51:02 +0200

    Delete print

--- 26.1.py
+++ 26.1.py
@@ -12,13 +12,13 @@ P_ENC = r'\bcharset=(?P<ENC>.+)\b'
 def getencoding(http_file):
     '''Отримати кодування файлу http_file з Інтернет.'''
     headers = http_file.getheaders()    # отримати заголовки файлу
-   # print(headers)
+
     dct = dict(headers)                 # перетворити у словник
 
     content = dct.get('Content-Type','')# знайти 'Content-Type'
-    #print(content)
+
     mt = re.search(P_ENC, content)      # знайти кодування (після 'charset=' )
-    #print(mt.group())
+
     if mt:
         enc = mt.group('ENC').lower().strip() # виділити кодування
     elif 'html' in content:
